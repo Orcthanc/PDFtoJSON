4d33098dc89b34de25999cbdbf00082533c4c3a0
Orcthanc <samuel@knoethig.net>
2018-11-07 17:02:56 +0100

    Started experimenting with pdfs

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <PDFWriter/InputFile.h>
 #include <PDFWriter/PDFParser.h>
+#include <PDFWriter/PDFDictionary.h>
+#include <PDFWriter/MapIterator.h>
+#include <PDFWriter/PDFName.h>
+
+#include <map>
 
 int main( int argc, char** argv ){
 	if( argc < 2 ){
@@ -13,5 +18,15 @@ int main( int argc, char** argv ){
 
 	pdf_file.OpenFile( argv[1] );
 	parser.StartPDFParsing( pdf_file.GetInputStream() );
+
+	printf( "%f %lu\n", parser.GetPDFLevel(), parser.GetPagesCount() );
+
+	PDFDictionary *root = parser.GetTrailer();
+
+	MapIterator<PDFNameToPDFObjectMap> it = root->GetIterator();
+
+	while( it.MoveNext() ){
+		printf( "%s\n", it.GetKey()->GetValue().c_str() );
+	}
 	
 }
