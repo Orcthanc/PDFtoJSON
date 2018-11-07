d74ae274836a3bcecc3ee5938b00e07f3469887e
Orcthanc <samuel@knoethig.net>
2018-11-07 16:35:08 +0100

    Added a symlink ./include to let vim find the headers more easily

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,17 @@
-
+#include <stdio.h>
+#include <PDFWriter/InputFile.h>
+#include <PDFWriter/PDFParser.h>
 
 int main( int argc, char** argv ){
-	return 0;
+	if( argc < 2 ){
+		printf( "Usage: PDFtoJSON <path_to_pdf.pdf>\n" );
+		return 1;
+	}
+
+	InputFile pdf_file;
+	PDFParser parser;
+
+	pdf_file.OpenFile( argv[1] );
+	parser.StartPDFParsing( pdf_file.GetInputStream() );
+	
 }
