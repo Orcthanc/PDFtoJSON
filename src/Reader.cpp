a5429e034623ba05e02b8bfcaa03518fcff6ea39
Orcthanc <samuel@knoethig.net>
2018-11-13 00:42:20 +0100

    Outputting everything works great

--- src/Reader.cpp
+++ src/Reader.cpp
@@ -6,6 +6,7 @@
 #include <PDFWriter/PDFIndirectObjectReference.h>
 
 #include <map>
+#include <algorithm>
 
 enum EPDFFormObjectTValuesType{
 	#define VALUE( name ) e##name,
@@ -17,36 +18,46 @@ std::map<std::string, EPDFFormObjectTValuesType> stoT = {
 #include "pdfFormObjectTValues.inl"
 };
 
+static std::string sanitize( std::string string ){
+	std::replace( string.begin(), string.end(), ' ', '_' );
+	return string;
+}
+
 bool readObject( Character& character, PDFDictionary& object, PDFParser& parser, std::string prefix ){
 	if( !object.Exists( "T" )){
-		printf( "Found weird object\n" );
+		if( object.Exists( "V" )){
+			printf( "Found weird object:\t\t\t%s\n", ((PDFLiteralString*)object.QueryDirectObject( "V" ))->GetValue().c_str() );
+		}
 		return false;
 	}
 
 
-	auto s_key = ((PDFLiteralString*)object.QueryDirectObject( "T" ))->GetValue();
+	auto s_key = sanitize( ((PDFLiteralString*)object.QueryDirectObject( "T" ))->GetValue() );
 
 	if( object.Exists( "Kids" )){
-		return readArr( character, *(PDFArray*)object.QueryDirectObject( "Kids" ), parser, prefix + s_key + "." );
+		return readArr( character, *(PDFArray*)object.QueryDirectObject( "Kids" ), parser, prefix + s_key + "__" );
 		
 	}
+	
+	if( !object.Exists( "V" ) ){
+//		printf( "Dafaq? Probably found an incorrect form.\n" );
+		return false;
+	}
+
+	std::string value = std::string(((PDFLiteralString*)object.QueryDirectObject( "V" ))->GetValue());
 
 	auto key = stoT.find( s_key );
 	if( key == stoT.end() ){
-		printf( "Keyword %s%s not found\n", prefix.c_str(), s_key.c_str() );
+		printf( "Keyword %s%s not found:\t\t%s\n", prefix.c_str(), s_key.c_str(), sanitize( value ).c_str() );
 		return false;
 	}
 	switch( key->second ){
 	case eclass:
-		if( !object.Exists( "V" ) ){
-			printf( "Dafaq? Probably found an incorrect form.\n" );
-		}
-		printf( "%i\n", object.QueryDirectObject( "V" )->GetType() );
 		if( character.classes.size() == 0 ){
 			printf( "Created class\n" );
 			character.classes.push_back( new DND_Class() );
 		}
-		character.classes[0]->name = std::string(((PDFLiteralString*)object.QueryDirectObject( "V" ))->GetValue());
+		character.classes[0]->name = value;
 		return true;
 	default:
 		return false;
@@ -62,9 +73,10 @@ bool readArr( Character& character, PDFArray& arr, PDFParser& parser, std::strin
 		if( obj->GetType() == PDFObject::ePDFObjectIndirectObjectReference ){
 			obj = parser.ParseNewObject( ((PDFIndirectObjectReference*)obj)->mObjectID );
 		}
-		if( !readObject( character, *(PDFDictionary*)obj
-					, parser, prefix ) )
+		if( !readObject( character, *(PDFDictionary*)obj, parser, prefix ) )
 			ret = false;
 	}
 	return ret;
 }
+
+
